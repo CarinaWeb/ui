cf3456611140d8f3927ea3ef2cd18b6c84247033
KathrynBrusewitz <kathryn.brusewitz@gmail.com>
2019-12-31 00:39:30 +0100

    Add dark mode

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,39 @@
-import { configure } from "@storybook/react";
-import { addDecorator } from "@storybook/react";
+import { configure, addDecorator, addParameters } from "@storybook/react";
+import { themes } from "@storybook/theming";
 import ThemeDecorator from "./decorators/ThemeDecorator";
+
+const carina = {
+  logo: {
+    primary: "https://dnox5b6qypgn8.cloudfront.net/carina-primary-logo.svg",
+    secondary: "https://dnox5b6qypgn8.cloudfront.net/carina-secondary-logo.png",
+  },
+  branding: {
+    brandTitle: "Carina Storybook",
+    brandUrl: "https://carina.org",
+  },
+};
+
+const theme = {
+  light: {
+    ...themes.light,
+    ...carina.branding,
+    brandImage: carina.logo["primary"],
+  },
+  dark: {
+    ...themes.dark,
+    ...carina.branding,
+    brandImage: carina.logo["secondary"],
+  },
+};
+
+addParameters({
+  darkMode: {
+    dark: theme["dark"],
+    light: theme["light"],
+  },
+});
+
+addDecorator(ThemeDecorator);
 
 // automatically import all files ending in *.stories.js
 configure(require.context("../src/stories", true, /\.stories\.js$/), module);
-
-addDecorator(ThemeDecorator);
